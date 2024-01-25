fca95ba6a699b34bb1e93bb8d737b6bd39bfbfe8
FidanHuseynzada <tu7m7m7rr@code.edu.az>
2024-01-25 10:55:18 +0400

    Main Done

---
+++ FinalExam/Services/LayoutService.cs
@@ -0,0 +1,21 @@
+﻿using FinalExam.DAL;
+
+namespace FinalExam.Services
+{
+    public class LayoutService
+    {
+        private readonly AppDbContext _context;
+
+        public LayoutService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //public async Task<Dictionary<string, string>> GetSettingAsync()
+        //{
+        //    Dictionary<string, string> settings = await _context.Settings.ToDictionaryAsync(s => s.Key, s => s.Value);
+        //    return settings;
+        //}
+
+    }
+}
--- FinalExam/obj/Debug/net6.0/FinalExam.AssemblyInfo.cs
+++ FinalExam/obj/Debug/net6.0/FinalExam.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalExam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c6379b3ece4912022f7300faabb2e9615b73ac2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cecdb7221e72f472ea018cfce32570eeea6c4ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalExam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalExam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
